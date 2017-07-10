099ab7354ce25cece6e1bc3fe4c108f4df7fc53b
Johan Holthausen <jaawholt@gmail.com>
2017-07-10 16:56:19 +0200

    Update and rename CMD Code Snippet.h to SIMD Code Snippet.h

--- SIMD Code Snippet.h
+++ SIMD Code Snippet.h
@@ -1,7 +1,7 @@
 
 /*
 In this function i check against world collisions using CMD
-with the use of CMD instructions i check 4 objects at the same time and apply a null mask the ones that are not relevant to continue the cycle.
+with the use of SIMD instructions i check 4 objects at the same time and apply a null mask the ones that are not relevant to continue the cycle.
 */
 
 
